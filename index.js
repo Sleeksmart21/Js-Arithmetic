e8b661de4e332ce856c00be06464de81462e996e
ChigozieOgbonna <ogbonnachigozieben@gmail.com>
2022-06-22 23:54:52 -0500

    My calculator

---
+++ index.js
@@ -0,0 +1,37 @@
+function makeItWork() {
+  var x,y,z,mat;
+  x=prompt("Give me your first number");
+  y=prompt("Give me another number");
+  mat=prompt("Type an operator");
+  
+  if(mat=="+"){
+    z=Number(x)+Number(y);
+  }
+  else if(mat=!null){
+    z="Error"
+  }
+  else if(x=!null){
+    z="Error"
+  }
+  else if(y=!null){
+    z="Error"
+  }
+  else if(mat=="/"){
+    z=Number(x)/Number(y);
+  }
+  else if(mat=="*"){
+    z=Number(x)*Number(y);
+  }
+  else if(mat=="-"){
+    z=Number(x)-Number(y);
+  }
+  
+  alert(z)
+    }
+// document.getElementById("demo").innerHTML =myFunction();
+
+
+
+      
+      
+  
